d1f648924ac1ef296b8da10f25b09ecbfa882a36
Iain Merrick <iain@iainmerrick.com>
2017-08-18 13:31:27 -0500

    Optimized cylinder -- reuse vertex data where possible

--- More.Shapes/Cylinder.cs
+++ More.Shapes/Cylinder.cs
@@ -22,46 +22,54 @@ namespace More.Shapes
 			x.Add(x[0]);
 			z.Add(z[0]);
 
-			float yhi = 0.5f;
-			float ylo = -0.5f;
+			const float yhi = 0.5f;
+			const float ylo = -0.5f;
 
 			// Top cap
 			{
-				float nx = 0, ny = 1, nz = 0;
+				int rim = mesh.VertexCount;
+				int center = rim + facets;
 				for (int i = 0; i < facets; ++i)
-				{
-					mesh.Add(0, yhi, 0, nx, ny, nz);
-					mesh.Add(x[i], yhi, z[i], nx, ny, nz);
-					mesh.Add(x[i + 1], yhi, z[i + 1], nx, ny, nz);
-				}
+					mesh.AddTri(center, rim + i, rim + (i + 1) % facets);
+
+				const float nx = 0, ny = 1, nz = 0;
+				for (int i = 0; i < facets; ++i)
+					mesh.AddVertex(x[i], yhi, z[i], nx, ny, nz);
+				mesh.AddVertex(0, yhi, 0, nx, ny, nz);
 			}
 
 			// Sides
 			{
+				int start = mesh.VertexCount;
+				int n = 2 * facets;
+				for (int i = 0; i < n; i += 2)
+				{
+					mesh.AddTri(start + i, start + i + 1, start + (i + 2) % n);
+					mesh.AddTri(start + (i + 3) % n, start + (i + 2) % n, start + i + 1);
+				}
+
 				var nx = x.Select((_) => 2 * _).ToArray();
 				var nz = z.Select((_) => 2 * _).ToArray();
 				for (int i = 0; i < facets; ++i)
 				{
-					mesh.Add(x[i], yhi, z[i], nx[i], 0, nz[i]);
-					mesh.Add(x[i], ylo, z[i], nx[i], 0, nz[i]);
-					mesh.Add(x[i + 1], yhi, z[i + 1], nx[i + 1], 0, nz[i + 1]);
-
-					mesh.Add(x[i + 1], ylo, z[i + 1], nx[i + 1], 0, nz[i + 1]);
-					mesh.Add(x[i + 1], yhi, z[i + 1], nx[i + 1], 0, nz[i + 1]);
-					mesh.Add(x[i], ylo, z[i], nx[i], 0, nz[i]);
+					mesh.AddVertex(x[i], yhi, z[i], nx[i], 0, nz[i]);
+					mesh.AddVertex(x[i], ylo, z[i], nx[i], 0, nz[i]);
 				}
 			}
 
 			// Bottom cap
 			{
-				float nx = 0, ny = -1, nz = 0;
+				int rim = mesh.VertexCount;
+				int center = rim + facets;
 				for (int i = 0; i < facets; ++i)
-				{
-					mesh.Add(0, ylo, 0, nx, ny, nz);
-					mesh.Add(x[i + 1], ylo, z[i + 1], nx, ny, nz);
-					mesh.Add(x[i], ylo, z[i], nx, ny, nz);
-				}
+					mesh.AddTri(center, rim + (i + 1) % facets, rim + i);
+
+				const float nx = 0, ny = -1, nz = 0;
+				for (int i = 0; i < facets; ++i)
+					mesh.AddVertex(x[i], ylo, z[i], nx, ny, nz);
+				mesh.AddVertex(0, ylo, 0, nx, ny, nz);
 			}
+
 			return mesh;
 		}
 	}
--- More.Shapes/Mesh.cs
+++ More.Shapes/Mesh.cs
@@ -5,14 +5,20 @@ namespace More.Shapes
 {
 	public class Mesh
 	{
+		public int VertexCount => Positions.Count / 3;
+
 		public List<int> Indices = new List<int>();
 		public List<float> Positions = new List<float>();
 		public List<float> Normals = new List<float>();
 		//List<float> TexCoords = new List<float>();
 
-		public void Add(float x, float y, float z, float nx, float ny, float nz)
+		public void AddTri(int i, int j, int k)
 		{
-			Indices.Add(Indices.Count);
+			Indices.Add(i, j, k);
+		}
+
+		public void AddVertex(float x, float y, float z, float nx, float ny, float nz)
+		{
 			Positions.Add(x, y, z);
 			Normals.Add(nx, ny, nz);
 		}
